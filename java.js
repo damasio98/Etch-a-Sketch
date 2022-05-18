7e53ff4172119eb6ddf6fb0a618d0b9a88e86fd0
José Damásio <jdpft17@gmail.com>
2022-05-18 15:57:22 +0100

    Included 'clear', 'erase' and 'rainbow' buttons.

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ const container = document.getElementById("container")
 //Creates Slider
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerHTML = slider.value + ' x ' + slider.value; // Display the default slider value
 
 //Creates Grid
 function makeGrid(rows, cols){
@@ -30,7 +30,7 @@ function reset() {
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     reset()
-    output.innerHTML = this.value;
+    output.innerHTML = this.value + ' x ' + this.value;
     rows = slider.value;
     cols = slider.value;
     makeGrid(rows,cols)
@@ -47,15 +47,63 @@ function size(){
 
 
 //Makes the background color change when hovered
+let color = 'black'
 const body = document.getElementById("body")
 const gridItem = document.getElementsByClassName("grid-item");
 body.addEventListener("mouseover",event =>{ 
     for (j = 0; j < rows * cols; j++){
-        if (event.target === gridItem[j]){
-            event.target.setAttribute('style', 'background: orange;');
+        if (event.target === gridItem[j] && color==='#'){
+            event.target.setAttribute('style', 'background:' + getRandomColor());
+            color='#'
         }
+        else if (event.target === gridItem[j]){
+            event.target.setAttribute('style', 'background:' + color);
+        }
+        
     }
     
 })
 
+//Butttons
+const black = document.getElementById("black")
+const rainbow = document.getElementById("rainbow")
+const erase = document.getElementById("erase")
+const clear = document.getElementById("clear")
+
+black.addEventListener("click",b1)
+
+rainbow.addEventListener("click",b2)
+
+erase.addEventListener("click", b3)
+
+//Cleans Grid
+clear.addEventListener("click", event => {
+    for (j = 0; j < rows * cols; j++){
+        gridItem[j].setAttribute('style', 'background: white');
+    }
+})
+
+//Changes background color
+function b1(){
+    color = 'black' 
+}
+
+function b2(){
+    color='#'
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+function b3(){
+    color = 'white' 
+}
+
+
+/*Math.floor(Math.random()*16777215).toString(16)*/
 
